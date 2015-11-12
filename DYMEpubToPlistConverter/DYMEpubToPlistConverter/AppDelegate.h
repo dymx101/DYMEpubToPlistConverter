6f31b5a302c469f8315fda2575b4d0ada340e531
Daniel Dong <dymx101@hotmail.com>
2015-11-12 10:52:25 +0800

    initial commit

---
+++ DYMEpubToPlistConverter/DYMEpubToPlistConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYMEpubToPlistConverter
+//
+//  Created by Dong Yiming on 15/11/12.
+//  Copyright © 2015年 Dong Yiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYMEpubToPlistConverter/DYMEpubToPlistConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYMEpubToPlistConverter
+//
+//  Created by Dong Yiming on 15/11/12.
+//  Copyright © 2015年 Dong Yiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
